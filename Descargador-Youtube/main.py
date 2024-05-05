41a758b2b785797daa0df1f10a044b5628c2b47c
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-05-05 18:00:57 -0300

    Update main.py

--- Descargador-Youtube/main.py
+++ Descargador-Youtube/main.py
@@ -4,6 +4,7 @@ import os
 from pytube import YouTube
 from pytube.exceptions import RegexMatchError, VideoUnavailable
 from moviepy.editor import VideoFileClip
+from tkinter import filedialog
 
 
 
@@ -13,46 +14,48 @@ def startDownload():
         ytLink = link.get()
         ytObject = YouTube(ytLink, on_progress_callback=on_progress)
         
+        download_folder = folder_path_var.get()  # Get the selected download folder
 
-        if download_choice.get() == "Video MP4":
-            video = ytObject.streams.get_highest_resolution()
-            file_extension = video.mime_type.split("/")[-1]
-            filename = video.default_filename
-        else:
-            video = ytObject.streams.filter(only_audio=True).first()
-            file_extension = "mp3"
-            filename = ytObject.title + ".mp3"
+        if download_folder:
+            if download_choice.get() == "Video MP4":
+                video = ytObject.streams.get_highest_resolution()
+                file_extension = video.mime_type.split("/")[-1]
+                filename = os.path.join(download_folder, video.default_filename)
+            else:
+                video = ytObject.streams.filter(only_audio=True).first()
+                file_extension = "mp3"
+                filename = os.path.join(download_folder, ytObject.title + ".mp3")
 
-        if video:
-            title_label.configure(text=ytObject.title, text_color="white")
-            finishLabel.configure(text="")
-            
-            video.download(filename=(video.default_filename.replace(".mp4", "") + "." + file_extension))
+            if video:
+                title_label.configure(text=ytObject.title, text_color="white")
+                finishLabel.configure(text="")
+                
+                video.download(output_path=download_folder, filename=(video.default_filename.replace(".mp4", "") + "." + file_extension))
 
-            if download_choice.get() == "Audio MP3":
-
-                if convert_to_mp3("temp." + file_extension, filename):
-                    os.remove("temp." + file_extension)
+                if download_choice.get() == "Audio MP3":
+                    if convert_to_mp3(os.path.join(download_folder, "temp." + file_extension), filename):
+                        os.remove(os.path.join(download_folder, "temp." + file_extension))
+                        print("Descarga Completada")
+                        finishLabel.configure(text="Descargado!")
+                else:
+                    os.rename(os.path.join(download_folder, "temp." + file_extension), filename)
                     print("Descarga Completada")
                     finishLabel.configure(text="Descargado!")
             else:
-                os.rename("temp." + file_extension, filename)
-                print("Descarga Completada")
-                finishLabel.configure(text="Descargado!")
-        
-        
+                print("no tiene resolucion adecuada")
+                finishLabel.configure(text="No tiene resolucion adecuada", text_color="red")  
         else:
-            print("no tiene resolucion adecuada")
-            finishLabel.configure(text="No tiene resolucion adecuada", text_color="red")  
-
+            print("Seleccione una carpeta de descarga")
+            finishLabel.configure(text="Seleccione una carpeta de descarga", text_color="red")
                                 
     except RegexMatchError:
-            print("enlace no es valido")
-            finishLabel.configure(text="Error al descargar, Link invalido", text_color="red")
+        print("enlace no es valido")
+        finishLabel.configure(text="Error al descargar, Link invalido", text_color="red")
     except VideoUnavailable:
-            print("video no disponible o enlace incorrecto")
-            finishLabel.configure(text="video no disponible o enlace incorrecto", text_color="red")
-        
+        print("video no disponible o enlace incorrecto")
+        finishLabel.configure(text="video no disponible o enlace incorrecto", text_color="red")
+
+
 def on_progress(stream, chunk, bytes_remaining):
     total_size = stream.filesize 
     bytes_download = total_size - bytes_remaining
@@ -68,6 +71,15 @@ def convert_to_mp3(input_file, output_file):
     video = VideoFileClip(input_file)
     video.audio.write_audiofile(output_file)
     video.close()
+
+def choose_folder():
+     folder_selected = filedialog.askdirectory()
+     folder_path_var.set(folder_selected)
+
+#def choose_resolution():
+#    resolution_selected = resolutions_choice.get()
+#    print("resolution selected:", resolution_selected)
+
 
 
 # configuracion 
@@ -116,14 +128,34 @@ download_menu.pack(padx=10, pady=10)
 #download_menu.place(relx=0.76, rely=0.26, anchor=tkinter.W)
 #relx= costado; rely= altura
 
+folder_path_var = tkinter.StringVar() # defino una variable para guardar la ruta elejida
+folder_label = customtkinter.CTkLabel(app, text="Carpeta de destino") #creo que prompt para que el usuario elija la ruta
+folder_label.pack()
+
+folder_path_entry = customtkinter.CTkEntry(app, textvariable=folder_path_var, state="readonly", width=100) 
+folder_path_entry.pack()
+
+#creo el boton 
+browse_button = customtkinter.CTkButton(app, text="Buscar", command=choose_folder)
+browse_button.pack(padx=10, pady=10)
+
 # termino la descarga 
 finishLabel = customtkinter.CTkLabel(app, text="")
 finishLabel.pack()
 
+# selector de resolucion
+resolutions = ["720p", "480p", "360p"]
+
+resolutions_var = tkinter.StringVar(app)
+resolutions_combobox = tkinter.Combobox(link_frame, values= resolutions, text_variable=resolutions_var )
+resolutions_combobox.pack(pady=("10p" , "5p"))
+resolutions_combobox.set("720px")
 
 # botton de descarga 
 download = customtkinter.CTkButton(app, text="Descargar", command=startDownload) 
 download.pack(padx=5, pady=5)
+
+
 
 
 # corro la app
