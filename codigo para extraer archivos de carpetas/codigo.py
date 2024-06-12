0d48124dce3f5185bad74d2bcab5f8462c65f49f
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-06-12 18:13:55 -0300

    Create codigo.py

---
+++ codigo para extraer archivos de carpetas/codigo.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# Use the absolute path for the folder
+folder = r""
+
+def move_files_to_main_folder(src_folder, dst_folder):
+    for item in os.listdir(src_folder):
+        src_path = os.path.join(src_folder, item)
+        if os.path.isdir(src_path):
+            # Recursively move contents of subdirectory
+            move_files_to_main_folder(src_path, dst_folder)
+            # Remove the empty subdirectory
+            os.rmdir(src_path)
+        else:
+            dst_path = os.path.join(dst_folder, item)
+            try:
+                logging.info(f'Moving {src_path} to {dst_path}')
+                shutil.move(src_path, dst_path)
+            except Exception as e:
+                logging.error(f'Error moving {src_path} to {dst_path}: {e}')
+
+# Initial call to move files from all subdirectories of the main folder
+subfolders = [f.path for f in os.scandir(folder) if f.is_dir()]
+
+for sub in subfolders:
+    move_files_to_main_folder(sub, folder)
